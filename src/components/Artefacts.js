bad8af3d3a4eb10117e080cbf469846a75e4f906
Gustavo_Martins <gtavomartins893@gmail.com>
2022-06-30 19:27:07 -0300

    created button to download artefacts

---
+++ src/components/Artefacts.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Artefacts = () => {
+  return (
+    <section id="features" className="grey lighten-4">
+      <div className="container">
+        <div className="row">
+          <div className="col m3"></div>
+          <div className="col m6 center-align">
+            <h2>Artefatos Construídos</h2>
+            
+            <a href="https://www.mediafire.com/folder/1hh4pdqp2gw7h/Artefatos+Sprint">
+                <button className="btn"> Baixar Artefatos</button>
+            </a>
+          </div>
+        </div>
+        
+      </div>
+    </section>
+  );
+};
+
+export default Artefacts;
